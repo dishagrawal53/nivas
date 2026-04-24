94204495b43594d5dd658f9407d48ffaebe60bd4
dishagrawal53 <dishaagrawal0503@gmail.com>
2026-04-24 21:24:30 +0530

    changed

--- middleware.js
+++ middleware.js
@@ -1,7 +1,7 @@
 const Listing = require("../models/listing.js");
 const Review = require("../models/review.js");
 const ExpressError = require("../utils/ExpressError.js");
-const {listingSchema,reviewSchema}=require("D:/projects/wanderlust/schema.js");
+const { listingSchema, reviewSchema } = require("./schema.js");
 module.exports.isLoggedIn=(req,res,next)=>{
     if(!req.isAuthenticated()){
         req.session.redirectUrl=req.originalUrl;
